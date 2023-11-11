4da3650ae31e04d5ec6e040cd30d19fb6bd8407d
Alder Junio <alder.junio@gmail.com>
2023-11-11 19:36:58 -0300

    criado component home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuizzComponent } from './components/quizz/quizz.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuizzComponent
+    QuizzComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
